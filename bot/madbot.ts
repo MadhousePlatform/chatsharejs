5354b56e4336c0b2f06620393b009e69c64724ff
Sketch <sketch@sketchni.uk>
2025-02-08 14:47:47 +0000

    Solution???

--- bot/madbot.ts
+++ bot/madbot.ts
@@ -1,8 +1,5 @@
-import { Client, Events, GatewayIntentBits, Message } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { log } from '@/bootstrap.ts';
-import docker from "dockerode";
-import Docker from "$/Docker.ts";
-import { gather_servers } from "@/index.ts";
 
 const client = new Client({
   intents: [
@@ -14,19 +11,6 @@ const client = new Client({
 client.once(Events.ClientReady, (readyClient: Client<true>) => {
   log.info(`Logged in as ${readyClient.user.tag}`)
 });
-
-client.on('messageCreate', async (message: Message) => {
-  if (message.author.bot) return;
-
-  const cmd = `tellraw @a [{"text":"[discord] ","color":"blue"},{"text":"<${message.author.username}> ","color":"light_purple"},{"text":"${message.content}","color":"white"}]\n`;
-
-  const servers = await gather_servers();
-
-  const dkr = new docker({
-    socketPath: '/var/run/docker.sock'
-  });
-  (new Docker()).broadcastToAll(cmd, servers.my_servers, dkr, null)
-})
 
 // @ts-ignore
 export {
--- classes/Docker.ts
+++ classes/Docker.ts
@@ -2,8 +2,9 @@ import InternalServer from "&/InternalServer.ts";
 import docker, { Container } from 'dockerode';
 import { client } from "../bot/madbot.ts";
 import { config, flog } from "@/bootstrap.ts";
-import { TextChannel } from "discord.js";
+import { Events, Message, TextChannel } from "discord.js";
 import Parser from "$/Parser.ts";
+import { gather_servers } from "@/index.ts";
 
 export default class Docker {
   handle_server(server: InternalServer, parser: Parser, all_servers: InternalServer[]): void {
@@ -11,6 +12,16 @@ export default class Docker {
       socketPath: '/var/run/docker.sock'
     });
     let dckr: Container = cntnr.getContainer(server.cid);
+
+    client.on(Events.MessageCreate, async (message: Message) => {
+      if (message.author.bot || message.channelId !== config.DISCORD_CHANNEL) return;
+
+      const cmd = `tellraw @a [{"text":"[discord] ","color":"blue"},{"text":"<${message.author.username}> ","color":"light_purple"},{"text":"${message.content}","color":"white"}]\n`;
+
+      const servers = await gather_servers();
+
+      this.broadcastToAll(cmd, servers.my_servers, cntnr, null)
+    })
 
     dckr.attach({
       stream: true,
